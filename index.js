db172054888e7a2b664fd65d5be72dc4efdfe4e9
AymenAhmed27 <135724911+AymenAhmed27@users.noreply.github.com>
2025-07-10 02:25:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ app.post('/login', async (req, res) => {
     const result = await pool.query('SELECT * FROM users WHERE phone = $1', [phone]);
     if (result.rows.length === 0) return res.status(404).json({ error: 'رقم الهاتف غير مسجل' });
     res.json(result.rows[0]);
-  } catch {
+  } catch (err) {
+    console.error('❌ Login Error:', err.message);
     res.status(500).json({ error: 'DB Error' });
   }
 });
